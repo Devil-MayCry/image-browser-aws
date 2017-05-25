9ed661875387065cee3c22df4d47202d7f2f3656
huteng <huteng@gagogroup.com>
2017-05-25 13:57:42 +0800

    refactor(docker): mount dir in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM docker.gagogroup.cn:5000/cent7:node
 
+VOLUME ["/home/ec2-user/s3-gagobucket"]
+
 # Add all files to /usr/local/image-browser-aws/
 WORKDIR /usr/local/
 RUN mkdir image-browser-aws/
--- bin/build_docker_image.sh
+++ bin/build_docker_image.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 read -p "Current VERSION is ? (0.1.0):" VERSION
 
+# pull master 
+
+git pull origin master
+
 # build image from docker file
 docker build -t docker.gagogroup.cn:5000/image-browser-aws:${VERSION} -t docker.gagogroup.cn:5000/image-browser-aws:latest .
 

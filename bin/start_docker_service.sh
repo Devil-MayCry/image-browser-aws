6415c362c55143355a6fa2edd9e16e325c516665
huteng <huteng@gagogroup.com>
2017-05-25 10:21:00 +0800

    fix(bin): fix start_docker_service script mount dir bug

--- bin/start_docker_service.sh
+++ bin/start_docker_service.sh
@@ -1,3 +1,3 @@
 # start docker and run the service bash script to run the service
 
-docker run -d -v s3-gagobucket docker.gagogroup.cn:5000/image-browser-aws /usr/local/image-browser-aws/bin/start_from_docker.sh
+docker run -d -v /home/ec2-user/s3-gagobucket docker.gagogroup.cn:5000/image-browser-aws /usr/local/image-browser-aws/bin/start_from_docker.sh
